48d0db9bb205fb8d8d5f37aaca06f1eed2c2d3e5
raysapida <ray.sapida@gmail.com>
2014-09-19 18:30:11 -0700

    Added status validations and tests

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -17,6 +17,12 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 	
+	test "should be logged in to post a status" do
+    post :create, status: { content: "Hello" }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+	
   test "should get new" do
     get :new
     assert_response :redirect
@@ -43,6 +49,12 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+	test "should redirect status update when not logged in" do
+    put :update, id: @status, status: { content: @status.content }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+	
   test "should update status when logged in" do
 		sign_in users(:jason)
     patch :update, id: @status, status: { content: @status.content}
