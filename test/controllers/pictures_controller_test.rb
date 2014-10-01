bc19f40d5f21e574dec4dfae87413a93fe2b16d7
raysapida <ray.sapida@gmail.com>
2014-10-01 06:16:17 -0700

    Fixed pictures controller test and added fixtures for them

--- test/controllers/pictures_controller_test.rb
+++ test/controllers/pictures_controller_test.rb
@@ -3,47 +3,55 @@ require 'test_helper'
 class PicturesControllerTest < ActionController::TestCase
   setup do
     @picture = pictures(:one)
+		@album = albums(:vacation)
+		@user = users(:jason)
+		@default_params = { profile_name: @user.profile_name, album_id: @album.id }
   end
 
   test "should get index" do
-    get :index
+    get :index, @default_params
     assert_response :success
     assert_not_nil assigns(:pictures)
   end
 
   test "should get new" do
-    get :new
+		sign_in users(:jason)
+    get :new, @default_params
     assert_response :success
   end
 
   test "should create picture" do
+		sign_in users(:jason)
     assert_difference('Picture.count') do
-      post :create, picture: { album_id: @picture.album_id, caption: @picture.caption, description: @picture.description, user_id: @picture.user_id }
+      post :create, @default_params.merge(picture: { album_id: @picture.album_id, caption: @picture.caption, description: @picture.description, user_id: @picture.user_id })
     end
 
-    assert_redirected_to picture_path(assigns(:picture))
+    assert_redirected_to album_pictures_path(assigns(:album))
   end
 
   test "should show picture" do
-    get :show, id: @picture
+    get :show, @default_params.merge(id: @picture)
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @picture
+		sign_in users(:jason)
+    get :edit, @default_params.merge(id: @picture)
     assert_response :success
   end
 
   test "should update picture" do
-    patch :update, id: @picture, picture: { album_id: @picture.album_id, caption: @picture.caption, description: @picture.description, user_id: @picture.user_id }
-    assert_redirected_to picture_path(assigns(:picture))
+		sign_in users(:jason)
+    patch :update, @default_params.merge(id: @picture, picture: { album_id: @picture.album_id, caption: @picture.caption, description: @picture.description, user_id: @picture.user_id })
+    assert_redirected_to album_pictures_path(assigns(:album))
   end
 
   test "should destroy picture" do
+		sign_in users(:jason)
     assert_difference('Picture.count', -1) do
-      delete :destroy, id: @picture
+      delete :destroy, @default_params.merge(id: @picture)
     end
 
-    assert_redirected_to pictures_path
+    assert_redirected_to album_pictures_path
   end
 end
