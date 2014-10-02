bc3958d4b969585e2b9720bc89fefddd22696244
raysapida <ray.sapida@gmail.com>
2014-10-02 03:43:22 -0700

    Added handlebarsjs and created js feature for activity feed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,99 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+window.loadedActivities = [];
+
+var addActivity = function(item) {
+	var found = false;
+	for (var i = 0; i < window.loadedActivities.length; i++) {
+		if (window.loadedActivities[i].id == item.id){
+			var found = true;
+		}
+	}
+	
+	if (!found) {
+		window.loadedActivities.push(item);
+		window.loadedActivities.sort(function(a, b){
+			var returnValue;
+      if (a.created_at > b.created_at)
+        returnValue = -1;
+      if (a.created_at < b.created_at)
+        returnValue = 1;
+      if (a.created_at == b.created_at)
+        returnValue = 0;
+      return returnValue;
+		});
+	}
+	
+	
+	
+	return item;
+};
+
+var renderActivities = function() {
+	var source = $('#activities-template').html();
+	var template = Handlebars.compile(source);
+	var html = template({
+		activities: window.loadedActivities, 
+		count: window.loadedActivities.length
+	});
+	var $activityFeedLink = $('li#activity-feed');
+	
+	$activityFeedLink.addClass('dropdown');
+	$activityFeedLink.html(html);
+	$activityFeedLink.find('a.dropdown-toggle').dropdiwn();
+};
+
+var pollActivity = function() {
+	$.ajax({
+		url: Routes.activities_path({format: 'json', since: window.lastFetch}),
+		type: "GET",
+		dataType: "json",
+		success: function(data) {
+			window.lastFetch = Math.floor((new Date).getTime()/1000);
+			if (data.length > 0) {
+				for (var i=0; i < data.length; i++) {
+					addActivity(data[i]);
+				}
+				renderActivities();
+			}
+		}
+	});
+};
+
+Handlebars.registerHelper('activityFeedLink', function(){
+	return new Handlebars.SafeString(Routes.activities_path());
+});
+
+Handlebars.registerHelper('activityLink', function() {
+	var link, path, html;
+	var activity = this;
+	var linkText = activity.targetable_type.toLowerCase();
+	
+	switch (linkText) {
+    case "status":
+      path = Routes.status_path(activity.targetable_id);
+      break;
+    case "album":
+      path = Routes.album_path(activity.profile_name, activity.targetable_id);
+      break;
+    case "picture":
+      path = Routes.album_picture_path(activity.profile_name, activity.targetable.album_id, activity.targetable_id);
+      break;
+    case "userfriendship":
+      path = Routes.profile_path(activity.profile_name);
+      linkText = "friend";
+      break;
+  }
+	
+	if (activity.action === 'deleted') {
+		path ='#';
+	}
+	
+	html = "<li role='presentation'><a role='menuitem' tabindex='-1' href='" + path + "'>" + this.user_name + " " + this.action + " a " + linkText + ".</a></li>"
+	return new Handlebars.SafeString(html);
+});
+
+window.pollInterval = window.setInterval( pollActivity, 5000);
+pollActivity();
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -7,8 +7,28 @@ class Activity < ActiveRecord::Base
 	def self.for_user(user, options={}) 
 		options[:page] ||= 1
 		friend_ids = user.friends.map(&:id).push(user.id)
-		where("user_id in (?)", friend_ids).
-			order("created_at desc").
-			page(options[:page])
+		collection = where("user_id in (?)", friend_ids).
+			order("created_at desc")
+		if options[:since] && !options[:since].blank? 
+			since = DateTime.strptime( options[:since], '%s' )
+			collection = collection.where("created_at > ?", since) if since
+		end
+		collection.page(options[:page])
+	end
+	
+	def user_name 
+		user.full_name
+	end
+	
+	def profile_name 
+		user.profile_name
+	end
+	
+	def as_json(options={}) 
+		super(
+				only: [:action, :id, :targetable_id, :targetable_type, :created_at, :id],
+				include: :targetable,
+				methods: [:user_name, :profile_name]
+			).merge(options)
 	end
 end
