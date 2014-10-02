12d2dd28b96d13cc42887d2714ef4ab4c54df9eb
raysapida <ray.sapida@gmail.com>
2014-10-02 03:48:50 -0700

    Rescued ActiveRecord::RecordNotFound and rendered 404 page instead

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 	
+	rescue_from ActiveRecord::RecordNotFound, with: :render_404
 	
 	# added section but might not be needed for new devise version
 	before_action :configure_permitted_parameters, if: :devise_controller?
+	
 
+	
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :profile_name, :email, :password, :password_confirmation) }
   end  
+	
+	def render_404 
+		render file: 'public/404', status: :not_found
+	end
 end
