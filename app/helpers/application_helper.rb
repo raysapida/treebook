cb36f8979909afa401af8a02bd200f8bdc9b41e0
raysapida <ray.sapida@gmail.com>
2014-10-02 09:34:46 -0700

    Edited views for buttons and alignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,8 @@ module ApplicationHelper
 	def bootstrap_paperclip_picture(form, paperclip_object) 
 		if form.object.send("#{paperclip_object}?")
 			content_tag(:div, class: 'control-group') do 
-				content_tag(:label, "Current #{paperclip_object.to_s.titleize}", class: 'control-label') + 
-				content_tag(:div, class: 'controls') do 
+				content_tag(:label, "Current #{paperclip_object.to_s.titleize}", class: 'col-sm-2 control-label') + 
+				content_tag(:div, class: 'col-sm-10 controls') do 
 					image_tag form.object.send(paperclip_object).send(:url, :small)
 				end
 			end
