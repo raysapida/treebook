7ef0075540c27650a034aa60cdfe1ad4817986d7
Raymond Sapida <ray.sapida@gmail.com>
2014-10-24 22:55:42 +0000

    Ran rake rails update and tests and server problems are the same

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,10 +2,3 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
-#module Paperclip
-#  class MediaTypeSpoofDetector
-#    def spoofed?
-#      false
-#    end
-#  end
-# end
