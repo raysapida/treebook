a38c99e53f7260035f6a3925e717abb855706793
Raymond Sapida <ray.sapida@gmail.com>
2014-11-26 03:44:39 +0000

    Edited database yml and schema while troubleshooting heroku pg bugs

--- db/schema.rb
+++ db/schema.rb
@@ -13,58 +13,61 @@
 
 ActiveRecord::Schema.define(version: 20141029012310) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "activities", force: true do |t|
     t.integer  "user_id"
-    t.string   "action",          limit: 255
+    t.string   "action"
     t.integer  "targetable_id"
-    t.string   "targetable_type", limit: 255
+    t.string   "targetable_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "activities", ["targetable_id", "targetable_type"], name: "index_activities_on_targetable_id_and_targetable_type"
-  add_index "activities", ["user_id"], name: "index_activities_on_user_id"
+  add_index "activities", ["targetable_id", "targetable_type"], name: "index_activities_on_targetable_id_and_targetable_type", using: :btree
+  add_index "activities", ["user_id"], name: "index_activities_on_user_id", using: :btree
 
   create_table "albums", force: true do |t|
     t.integer  "user_id"
-    t.string   "title",      limit: 255
+    t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "albums", ["user_id"], name: "index_albums_on_user_id"
+  add_index "albums", ["user_id"], name: "index_albums_on_user_id", using: :btree
 
   create_table "documents", force: true do |t|
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "attachment_file_name",    limit: 255
-    t.string   "attachment_content_type", limit: 255
+    t.string   "attachment_file_name"
+    t.string   "attachment_content_type"
     t.integer  "attachment_file_size"
     t.datetime "attachment_updated_at"
   end
 
-  add_index "documents", ["user_id"], name: "index_documents_on_user_id"
+  add_index "documents", ["user_id"], name: "index_documents_on_user_id", using: :btree
 
   create_table "pictures", force: true do |t|
     t.integer  "album_id"
     t.integer  "user_id"
-    t.string   "caption",               limit: 255
+    t.string   "caption"
     t.text     "description"
-    t.string   "pictures_file_name",    limit: 255
-    t.string   "pictures_content_type", limit: 255
+    t.string   "pictures_file_name"
+    t.string   "pictures_content_type"
     t.integer  "pictures_file_size"
     t.datetime "pictures_updated_at"
-    t.string   "asset_file_name",       limit: 255
-    t.string   "asset_content_type",    limit: 255
+    t.string   "asset_file_name"
+    t.string   "asset_content_type"
     t.integer  "asset_file_size"
     t.datetime "asset_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "pictures", ["album_id"], name: "index_pictures_on_album_id"
-  add_index "pictures", ["user_id"], name: "index_pictures_on_user_id"
+  add_index "pictures", ["album_id"], name: "index_pictures_on_album_id", using: :btree
+  add_index "pictures", ["user_id"], name: "index_pictures_on_user_id", using: :btree
 
   create_table "statuses", force: true do |t|
     t.text     "content"
@@ -74,37 +77,37 @@ ActiveRecord::Schema.define(version: 20141029012310) do
     t.integer  "document_id"
   end
 
-  add_index "statuses", ["user_id"], name: "index_statuses_on_user_id"
+  add_index "statuses", ["user_id"], name: "index_statuses_on_user_id", using: :btree
 
   create_table "user_friendships", force: true do |t|
     t.integer  "user_id"
     t.integer  "friend_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "state",      limit: 255
+    t.string   "state"
   end
 
-  add_index "user_friendships", ["state"], name: "index_user_friendships_on_state"
-  add_index "user_friendships", ["user_id", "friend_id"], name: "index_user_friendships_on_user_id_and_friend_id"
+  add_index "user_friendships", ["state"], name: "index_user_friendships_on_state", using: :btree
+  add_index "user_friendships", ["user_id", "friend_id"], name: "index_user_friendships_on_user_id_and_friend_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "first_name",             limit: 255
-    t.string   "last_name",              limit: 255
-    t.string   "profile_name",           limit: 255
-    t.string   "email",                  limit: 255, default: "",    null: false
-    t.string   "encrypted_password",     limit: 255, default: "",    null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "profile_name"
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.boolean  "admin",                              default: false
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.boolean  "admin",                  default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
