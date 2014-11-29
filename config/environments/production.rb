857c3f929a54091cbaa8f4d74c4ac6bc1f230be5
Raymond Sapida <ray.sapida@gmail.com>
2014-11-29 04:12:51 +0000

    Edited production config to allow Rails to serve static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,4 +74,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+	config.serve_static_assets = true
+	config.action_dispatch.x_sendfile_header = nil
 end
