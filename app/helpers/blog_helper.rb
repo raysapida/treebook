8eb4f73b9c43fbb7939794f83a2a250310c1f0db
Raymond Sapida <ray.sapida@gmail.com>
2015-03-23 13:54:03 -0700

    Added helpers for pygments.rb

---
+++ app/helpers/blog_helper.rb
@@ -0,0 +1,24 @@
+module BlogHelper
+  class HTMLwithPygments < Redcarpet::Render::HTML
+    def block_code(code, language)
+      Pygments.highlight(code, lexer: language)
+    end
+  end
+
+  def markdown(text)
+    renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
+    options = {
+      autolink: true,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      lax_html_blocks: true,
+      strikethrough: true,
+      superscript: true
+    }
+    Redcarpet::Markdown.new(renderer, options).render(text).html_safe
+  end
+
+  def preserve_markdown(text) # Used to get the indentation right in the <pre> code blocks with Haml
+    preserve markdown(text)
+  end
+end
