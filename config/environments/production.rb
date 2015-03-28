255faa2229eb682079b9585e85536171e4a99f83
Raymond Sapida <ray.sapida@gmail.com>
2015-03-28 01:29:47 -0700

    Changed default host for mailer and devise sender.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
-  config.action_mailer.default_url_options = { :host => 'https://whispering-beach-6783.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'https://mustached-octo-robot.herokuapp.com/' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
