4511b45c2b9e1b11bf1ba66ff5c6460db9360b3f
Raymond Sapida <ray.sapida@gmail.com>
2015-03-28 02:25:25 -0700

    Undo last commit

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
-HOSTNAME = ENV['HOSTNAME']
-
 module SocialMedia
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
