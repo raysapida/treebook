0f926a236f8a2c28a9867f165c0c8868658782e8
Raymond Sapida <ray.sapida@gmail.com>
2015-04-01 06:49:43 -0700

    Added Devise test helpers and started profiles controller spec
    - Spec currently tests successful respones when signed in and 404
      when not signed in

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,7 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Paperclip::Shoulda::Matchers
+  config.include Devise::TestHelpers, :type => :controller
 
   config.use_transactional_fixtures = false
 
