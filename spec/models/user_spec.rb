247463191523476742d3c666e1dfa4726503657b
Raymond Sapida <ray.sapida@gmail.com>
2015-04-01 05:13:00 -0700

    Added more tests to user spec along with 3 pending ones
    - Tests #to_param, #full_name, and #friends
    - Pending tests for #has_blocked? and #create_activity

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 describe User do
-  it { should have_many(:statuses)  }
-  it { should have_many(:friends)  }
+  let(:user) { create(:user) }
+  let(:friend) { create(:user) }
+
+  it { should have_many(:statuses) }
+  it { should have_many(:friends) }
   it { should have_many(:user_friendships)  }
   it { should have_many(:pending_user_friendships)  }
   it { should have_many(:pending_friends)  }
@@ -17,9 +20,49 @@ describe User do
   it { should validate_presence_of(:first_name)}
   it { should validate_presence_of(:last_name)}
   it { should validate_presence_of(:profile_name)}
+  it { should validate_uniqueness_of(:profile_name)}
+
+  it { should_not allow_value('ex@mple').for(:profile_name) }
+  it { should_not allow_value('ex.ample').for(:profile_name) }
+  it { should_not allow_value('exam ple').for(:profile_name) }
 
   it 'has a valid factory' do
-    user_factory = FactoryGirl.create(:user)
-    expect(user_factory).to be_valid
+    expect(user).to be_valid
+  end
+
+  it '#to_param calls profile_name' do
+    expect(user.to_param).to eq(user.profile_name)
+  end
+
+  it '#full_name concatenates first_name and last_name' do
+    full = "#{user.first_name} #{user.last_name}"
+
+    expect(user.full_name).to eq(full)
+  end
+
+  it 'can call friends list' do
+    expect{ user.friends }.not_to raise_error
+  end
+
+	it 'that creating friendships on a user works' do
+    user.friends << friend
+    user.friends.reload
+
+    expect(user.friends).to include(friend)
+  end
+  
+  it '#has_blocked?' do
+    pending('needs a factory for user friendships')
+    expect(user.has_blocked?(friend)).to be true
+  end
+
+  it '#create_activity for status' do
+    pending('needs a factory for statuses')
+    expect(user.create_activity(status, activity)).to change(activity.count).by(1)
+  end
+
+  it '#create_activity for album' do
+    pending('needs a factory for statuses')
+    expect(user.create_activity(album, activity)).to change(activity.count).by(1)
   end
 end
