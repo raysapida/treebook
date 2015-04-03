f72cddb4f61a6f946c09a2f44380d503d214b51b
Raymond Sapida <ray.sapida@gmail.com>
2015-04-03 00:14:54 -0700

    Added more tests on picture controller spec and refactored
    - Extracted default_params and picture_params
    - Changed wording of it block to reflect which user is used if any
    - Separated tests clearly between setup, action, and verification
    - Added tests where the signed in user does not own the pictures

--- spec/controllers/pictures_controller_spec.rb
+++ spec/controllers/pictures_controller_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
 describe PicturesController do
-  let(:user) { create(:user) }
-  let(:album) { create(:album) }
-  let(:picture) { create(:picture) }
+  let!(:different_user) { create(:user) }
+  let!(:picture) { create(:picture) }
+  let!(:default_params) {  {profile_name: picture.album.user, album_id: picture.album} }
+  let!(:picture_params) do 
+    {
+      picture: {user_id: picture.album.user, 
+                album_id: picture.album, 
+                caption: picture.caption, 
+                description: picture.description}
+    }
+  end
 
   describe 'GET index' do
-    it 'with a signed in user' do
-      sign_in :user, album.user
-
-      get :index, profile_name: album.user, album_id: album
+    it 'without a signed in user' do
+      get :index, default_params
 
       expect(response).to be_success
       expect(response).to render_template(:index)
@@ -17,10 +23,8 @@ describe PicturesController do
   end
 
   describe 'GET show' do
-    it 'with a signed in user' do
-      sign_in :user, picture.album.user
-
-      get :show, profile_name: picture.album.user, album_id: picture.album, id: picture
+    it 'without a signed in user' do
+      get :show, default_params.merge(id: picture)
 
       expect(response).to be_success
       expect(response).to render_template(:show)
@@ -28,35 +32,81 @@ describe PicturesController do
   end
 
   describe 'GET new' do
-    it 'with a signed in user' do
+    it 'with the current signed in user' do
       sign_in :user, picture.album.user
-      get :new, profile_name: picture.album.user, album_id: picture.album.id, id: picture.id
+
+      get :new, default_params.merge(id: picture)
+
       expect(response).to be_success
       expect(response).to render_template(:new)
+    end
+
+    it 'with a different signed in user' do
+      sign_in :user, different_user
+
+      get :new, default_params.merge(id: picture)
+
+      expect(flash[:error]).to match(/You don't have permission to do that./)
+      expect(response).to redirect_to(album_pictures_path(picture.album))
     end
   end
 
   describe 'GET edit' do
-    it 'with a signed in user' do
+    it 'with the current signed in user' do
       sign_in :user, picture.album.user
-      get :edit, profile_name: picture.album.user, album_id: picture.album.id, id: picture.id
+
+      get :edit, default_params.merge(id: picture)
+      
       expect(response).to be_success
       expect(response).to render_template(:edit)
+    end
+
+    it 'with a different signed in user' do
+      sign_in :user, different_user
+
+      get :edit, default_params.merge(id: picture)
+
+      expect(flash[:error]).to match(/You don't have permission to do that./)
+      expect(response).to redirect_to(album_pictures_path(picture.album))
     end
   end
 
   describe 'POST create' do
-    it 'with a signed in user' do
+    it 'with the signed in user' do
       sign_in :user, picture.album.user
-      post :create, profile_name: picture.album.user.profile_name, album_id: picture.album.id , picture: {user_id: picture.album.user.id, album_id: picture.album.id, caption: picture.caption, description: picture.description}
+
+      post :create, default_params.merge(picture_params)
+
+      expect(flash[:notice]).to match(/Picture was successfully created./)
+      expect(response).to redirect_to(album_pictures_path(picture.album))
+    end
+
+    it 'with a different signed in user' do
+      sign_in :user, different_user
+
+      post :create, default_params.merge(picture_params)
+
+      expect(flash[:error]).to match(/You don't have permission to do that./)
       expect(response).to redirect_to(album_pictures_path(picture.album))
     end
   end
 
   describe 'PUT update' do
-    it 'with a signed in user' do
+    it 'with the current signed in user' do
       sign_in :user, picture.album.user
-      put :update, profile_name: picture.album.user,  album_id: picture.album.id, id: picture.id, picture: { user_id: picture.album.user.id, album_id: picture.album.id, caption: picture.caption, description: picture.description}
+
+      put :update, default_params.merge(picture_params).merge(id: picture)
+
+      expect(flash[:notice]).to match(/Picture was successfully updated./)
+      expect(response).to redirect_to(album_pictures_path(picture.album))
+    end
+
+    it 'with a different signed in user' do
+      sign_in :user, different_user
+
+      put :update, default_params.merge(picture_params).merge(id: picture)
+
+      expect(flash[:error]).to match(/You don't have permission to do that./)
       expect(response).to redirect_to(album_pictures_path(picture.album))
     end
   end
@@ -64,7 +114,19 @@ describe PicturesController do
   describe 'DELETE destroy' do
     it 'with a signed in user' do
       sign_in :user, picture.album.user
-      delete :destroy, profile_name: picture.album.user, album_id: picture.album.id, caption: picture.caption, description: picture.description, id: picture.id
+
+      delete :destroy, default_params.merge(id: picture)
+
+      expect(flash[:notice]).to match(/Picture was successfully destroyed./)
+      expect(response).to redirect_to(album_pictures_path(picture.album))
+    end
+
+    it 'with a different signed in user' do
+      sign_in :user, different_user
+
+      delete :destroy, default_params.merge(id: picture)
+
+      expect(flash[:error]).to match(/You don't have permission to do that./)
       expect(response).to redirect_to(album_pictures_path(picture.album))
     end
   end
