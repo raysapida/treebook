c82c8c4df1d699af8f1fb06f78b4dc171ebc1bd0
Raymond Sapida <ray.sapida@gmail.com>
2015-04-30 16:55:14 -0700

    Fix failing request specs

--- spec/requests/pictures_spec.rb
+++ spec/requests/pictures_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe "Pictures", type: :request do
   describe "GET /pictures" do
     it "works! (now write some real specs)" do
-      get pictures_path
+      picture = create(:picture)
+      get album_pictures_path(album_id: picture.album, profile_name: picture.album.user)
       expect(response).to have_http_status(200)
     end
   end
