def8f1e02acaf3b1730d80472933a176bc50eb10
Raymond Sapida <ray.sapida@gmail.com>
2015-04-30 17:13:51 -0700

    Edit pictures routing spec to include profile name and album id

--- spec/routing/pictures_routing_spec.rb
+++ spec/routing/pictures_routing_spec.rb
@@ -4,31 +4,56 @@ RSpec.describe PicturesController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/pictures").to route_to("pictures#index")
+      expect(:get => "/profilename/albums/1/pictures").to route_to(:controller => "pictures",
+                                                                   :action => "index",
+                                                                   :album_id => "1",
+                                                                   :profile_name => "profilename")
     end
 
     it "routes to #new" do
-      expect(:get => "/pictures/new").to route_to("pictures#new")
+      expect(:get => "/profilename/albums/1/pictures/new").to route_to(:controller => "pictures",
+                                                                       :action => "new",
+                                                                       :album_id => "1",
+                                                                       :profile_name => "profilename")
     end
 
     it "routes to #show" do
-      expect(:get => "/pictures/1").to route_to("pictures#show", :id => "1")
+      expect(:get => "/profilename/albums/1/pictures/1").to route_to(:controller => "pictures",
+                                                                     :action => "show",
+                                                                     :album_id => "1",
+                                                                     :id => "1",
+                                                                     :profile_name => "profilename")
     end
 
     it "routes to #edit" do
-      expect(:get => "/pictures/1/edit").to route_to("pictures#edit", :id => "1")
+      expect(:get => "/profilename/albums/1/pictures/1/edit").to route_to(:controller => "pictures",
+                                                                          :action => "edit",
+                                                                          :album_id => "1",
+                                                                          :id => "1",
+                                                                          :profile_name => "profilename")
     end
 
     it "routes to #create" do
-      expect(:post => "/pictures").to route_to("pictures#create")
+      expect(:post => "/profilename/albums/1/pictures").to route_to(:controller => "pictures",
+                                                                    :action => "create",
+                                                                    :album_id => "1",
+                                                                    :profile_name => "profilename")
     end
 
     it "routes to #update" do
-      expect(:put => "/pictures/1").to route_to("pictures#update", :id => "1")
+      expect(:put => "/profilename/albums/1/pictures/1").to route_to(:controller => "pictures",
+                                                                     :action => "update",
+                                                                     :album_id => "1",
+                                                                     :id => "1",
+                                                                     :profile_name => "profilename")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/pictures/1").to route_to("pictures#destroy", :id => "1")
+      expect(:delete => "/profilename/albums/1/pictures/1").to route_to(:controller => "pictures",
+                                                                        :action => "destroy",
+                                                                        :album_id => "1",
+                                                                        :id => "1",
+                                                                        :profile_name => "profilename")
     end
 
   end
