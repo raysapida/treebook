f50634656a286b9b0fa88f6c6e562e0a326a1521
Raymond Sapida <ray.sapida@gmail.com>
2015-05-02 14:52:28 -0700

    Add commented files to picture factory for attachment file

--- spec/factories/picture.rb
+++ spec/factories/picture.rb
@@ -3,8 +3,14 @@ require 'faker'
 FactoryGirl.define do
   factory :picture do |f|
     association :album, factory: :album
+    # asset { fixture_file_upload "#{Rails.root}/spec/images/rails.png", 'image/png' }
 
     f.caption { Faker::Lorem.sentence }
     f.description { Faker::Lorem.characters }
+
+    # In case of `Errno::EMFILE: Too many open files`
+    # after_create do |asset, proxy|
+    #   proxy.file.close
+    # end
   end
 end
