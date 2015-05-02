255674aa82f231933cf28eaa6d47ab572ffee14e
Raymond Sapida <ray.sapida@gmail.com>
2015-05-02 14:55:32 -0700

    Fix failing tests by adding corresponding module for fixture_file_upload
    https://github.com/thoughtbot/factory_girl/issues/385

--- spec/factories/picture.rb
+++ spec/factories/picture.rb
@@ -1,9 +1,11 @@
 require 'faker'
 
+include ActionDispatch::TestProcess
+
 FactoryGirl.define do
   factory :picture do |f|
     association :album, factory: :album
-    # asset { fixture_file_upload "#{Rails.root}/spec/images/rails.png", 'image/png' }
+    asset { fixture_file_upload "#{Rails.root}/spec/images/rails.png", 'image/png' }
 
     f.caption { Faker::Lorem.sentence }
     f.description { Faker::Lorem.characters }
