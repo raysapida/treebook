447475e563647eeefb26cfebafd6158b9f6159b5
Raymond Sapida <ray.sapida@gmail.com>
2015-05-01 18:36:12 -0700

    Edit feature spec that tests image uploading
    - Add an images directory to place image used for tests

--- spec/features/creating_statuses_spec.rb
+++ spec/features/creating_statuses_spec.rb
@@ -9,7 +9,7 @@ describe "creating a status", :type => :feature do
     click_button 'Log in'
     expect(page).to have_content 'Signed in successfully'
 
-    click_button 'Post a New Status'
+    click_link 'Post a New Status'
     expect(page).to have_content 'New Status Update'
     fill_in 'Content', :with => 'I hope this works'
     click_button 'Create Status'
@@ -17,17 +17,19 @@ describe "creating a status", :type => :feature do
     expect(page).to have_content 'I hope this works'
 
     click_link 'All Statuses'
-    click_button 'Post a New Status'
+    click_link 'Post a New Status'
     expect(page).to have_content 'New Status Update'
     fill_in 'Content', :with => 'A new hope'
     click_button 'Create Status'
     expect(page).to have_content 'Status was successfully created.'
+    expect(page).to have_content 'A new hope'
+
+    click_link 'All Statuses'
     expect(page).to have_content 'I hope this works'
     expect(page).to have_content 'A new hope'
   end
 
   it "can upload an image" do
-    pending("Need to figure out how to test paperclip's upload")
     user = create(:user)
     visit root_path
     fill_in 'Email', :with => user.email
@@ -36,12 +38,16 @@ describe "creating a status", :type => :feature do
     expect(page).to have_content 'Signed in successfully'
 
     click_link 'All Statuses'
-    click_button 'Post a New Status'
+    click_link 'Post a New Status'
     expect(page).to have_content 'New Status Update'
-    fill_in 'Content', :with => ''
+    fill_in 'Content', :with => 'Picture Upload'
+    attach_file("status[document_attributes][attachment]",
+                "#{Rails.root}/spec/images/rails.png")
     click_button 'Create Status'
-    expect(page).to have_content 'Status was successfully created.'
-    expect(page).to have_content 'I hope this works'
-    expect(page).to have_content 'A new hope'
+    expect(page).to have_content 'Picture Upload'
+    page.has_css?('img-polaroid')
+    # expect(page).to have_xpath("#{Rails.root}/spec/images/rails.png")
+    src = "#{Rails.root}/spec/images/rails.png"
+    page.has_xpath?("//img[contains(@src,\"/images/#{src}\")]")
   end
 end
