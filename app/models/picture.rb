90566b7822b6b2a31c0fd25ed46d806c9cf9e8d6
Raymond Sapida <ray.sapida@gmail.com>
2015-05-04 04:07:56 -0700

    Add presence validations in picture for user_id and album_id

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,8 @@
 class Picture < ActiveRecord::Base
   belongs_to :album
   belongs_to :user
+
+  validates_presence_of :user_id, :album_id
 	
 	has_attached_file :asset, styles: {
 		large: "800x800>", medium: "300x200>", small: "260x180>", thumb: "80x80#"
--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 describe Picture do
   it { should belong_to(:user) }
   it { should belong_to(:album) }
+
+  it { should validate_presence_of(:album_id) }
 end
