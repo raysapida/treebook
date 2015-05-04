b79e35b3fad7ed37a9a7d51cd852cd0b1a5e157f
Raymond Sapida <ray.sapida@gmail.com>
2015-05-04 04:29:27 -0700

    Add user_id presence validation test in picture model

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -4,5 +4,6 @@ describe Picture do
   it { should belong_to(:user) }
   it { should belong_to(:album) }
 
+  it { should validate_presence_of(:user_id) }
   it { should validate_presence_of(:album_id) }
 end
