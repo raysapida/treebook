3479c4c95e253be9bf89e6805fa4a073100f1ec8
Raymond Sapida <ray.sapida@gmail.com>
2015-05-04 16:43:26 -0700

    Edit devise parametes for sign up and update
    http://stackoverflow.com/questions/16297797/add-custom-field-column-to-devise-with-rails-4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,21 +2,32 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-	
-	rescue_from ActiveRecord::RecordNotFound, with: :render_404
-	
-	# added section but might not be needed for new devise version
-	before_action :configure_permitted_parameters, if: :devise_controller?
-	
 
-	
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
+  # added section but might not be needed for new devise version
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :profile_name, :email, :password, :password_confirmation) }
-  end  
-	
-	def render_404 
-		render file: 'public/404', status: :not_found
-	end
+    #devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :profile_name, :email, :password, :password_confirmation) }
+    registration_params = [:first_name, :last_name, :profile_name, :email, :password, :password_confirmation, :avatar]
+
+    if params[:action] == 'update'
+      devise_parameter_sanitizer.for(:account_update) {
+        |u| u.permit(registration_params << :current_password)
+      }
+    elsif params[:action] == 'create'
+      devise_parameter_sanitizer.for(:sign_up) {
+        |u| u.permit(registration_params)
+      }
+    end
+  end
+
+  def render_404
+    render file: 'public/404', status: :not_found
+  end
 end
