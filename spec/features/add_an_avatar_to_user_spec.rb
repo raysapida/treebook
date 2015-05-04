9b8e5cb293106092a8b3ad8438ac21279b63ad27
Raymond Sapida <ray.sapida@gmail.com>
2015-05-04 16:50:02 -0700

    TDD a feature on adding avatar image to user and showing it in the view

---
+++ spec/features/add_an_avatar_to_user_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe 'adding an avatar', :type => :feature do
+  it 'with a signed in user' do
+    user = create(:user)
+    status = create(:status, user: user)
+
+    visit root_path
+    fill_in 'Email', :with => user.email
+    fill_in 'Password', :with => user.password
+    click_button 'Log in'
+    expect(page).to have_content 'Signed in successfully'
+
+    click_link user.full_name
+    expect(page).to have_content 'Edit My Profile'
+
+    fill_in 'Current password', :with => user.password
+    attach_file('user[avatar]',
+                "#{Rails.root}/spec/images/rails.png")
+    click_button 'Update'
+    expect(page).to have_content 'Your account has been updated successfully.'
+
+    expect(page).to have_content user.full_name
+    src = "#{Rails.root}/spec/images/rails.png"
+    page.has_xpath?("//img[contains(@src,\"/images/#{src}\")]")
+
+    find('img.avatar').click
+    # Add expectations to this once profile view is more developed
+  end
+end
