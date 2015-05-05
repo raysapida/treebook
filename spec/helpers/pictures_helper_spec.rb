f6b542f740c7f030fd6bc402c0d6f2897d22ab2b
Raymond Sapida <ray.sapida@gmail.com>
2015-05-04 23:45:35 -0700

    Test that #can_edit_picture? method is valid

--- spec/helpers/pictures_helper_spec.rb
+++ spec/helpers/pictures_helper_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
+include Devise::TestHelpers
 
-# Specs in this file have access to a helper object that includes
-# the PicturesHelper. For example:
-#
-# describe PicturesHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe PicturesHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it '#can_edit_picture? works as intended' do
+    user = create(:user)
+    picture = create(:picture, user: user)
+
+    sign_in :user, user
+
+    expect(helper.can_edit_picture?(picture)).to be_truthy
+  end
 end
