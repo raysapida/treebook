9b65d8d8065095151e8e55e5856a7713f917ff39
Raymond Sapida <ray.sapida@gmail.com>
2015-05-07 02:32:44 -0700

    Use fill_form method is signin feature spec

--- spec/features/signin_process_spec.rb
+++ spec/features/signin_process_spec.rb
@@ -4,8 +4,7 @@ describe "the signin process", :type => :feature do
   it "works with a valid user" do
     user = create(:user)
     visit login_path
-    fill_in 'Email', :with => user.email
-    fill_in 'Password', :with => user.password
+    fill_form(:user, :new, { email: user.email, password: user.password})
     click_button 'Log in'
     expect(page).to have_content 'Signed in successfully'
   end
