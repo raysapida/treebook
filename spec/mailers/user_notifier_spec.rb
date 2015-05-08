76be06ee09b1bc32d2de0f47b682deaf1055d079
Raymond Sapida <ray.sapida@gmail.com>
2015-05-08 10:42:44 -0700

    Add tests for friend_request_accepted mailer method

--- spec/mailers/user_notifier_spec.rb
+++ spec/mailers/user_notifier_spec.rb
@@ -36,4 +36,34 @@ describe UserNotifier, type: :mailer do
     end
   end
 
+  context '#friend_request_accepted' do
+    before(:each) do
+      ActionMailer::Base.delivery_method = :test
+      ActionMailer::Base.perform_deliveries = true
+      ActionMailer::Base.deliveries = []
+      @friendship = create(:pending_user_friendship)
+      UserNotifier.friend_request_accepted(@friendship.id).deliver_now
+    end
+
+    after(:each) do
+      ActionMailer::Base.deliveries.clear
+    end
+
+    it 'should send an email' do
+      expect(ActionMailer::Base.deliveries.count).to eq(1)
+    end
+
+    it 'renders the receiver email' do
+      expect(ActionMailer::Base.deliveries.first.to).to eq([@friendship.user.email])
+    end
+
+    it 'should set the subject to the correct subject' do
+      expect(ActionMailer::Base.deliveries.first.subject).to eq("#{@friendship.friend.first_name} has accepted your friend request.")
+    end
+
+    it 'renders the sender email' do  
+      expect(ActionMailer::Base.deliveries.first.from).to eq(['from@example.com'])
+    end
+  end
+
 end
