eda802f7ff5ad76292adc27cf753a5dc91309da7
Raymond Sapida <ray.sapida@gmail.com>
2015-05-13 11:24:37 -0700

    Add specs for other other omniauth providers

--- spec/features/omniauth_spec.rb
+++ spec/features/omniauth_spec.rb
@@ -1,26 +1,79 @@
 require 'rails_helper'
 
-describe 'access top page', type: :feature do
-  before do
-    Rails.application.env_config['devise.mapping'] = Devise.mappings[:user]
-    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:twitter]
+describe 'logging in through social logins', type: :feature do
+  describe 'twitter omniauth' do
+    it 'can sign in user with Twitter account' do
+      visit '/'
+      expect(page).to have_content('Sign in with Twitter')
+      mock_auth_twitter
+      click_link 'Sign in with Twitter'
+      expect(page).to have_content('Successfully authenticated from Twitter account.')
+      expect(page).to have_content('Log Out')
+    end
+
+    it 'can handle authentication error' do
+      OmniAuth.config.mock_auth[:twitter] = :invalid_credentials
+      visit '/'
+      expect(page).to have_content('Sign in with Twitter')
+      click_link 'Sign in with Twitter'
+      expect(page).to have_content('Invalid credentials')
+    end
   end
 
-  it 'can sign in user with Twitter account' do
-    visit '/'
-    expect(page).to have_content('Sign in with Twitter')
-    mock_auth_hash
-    click_link 'Sign in with Twitter'
-    expect(page).to have_content('Successfully authenticated from Twitter account.')
-    expect(page).to have_content('Log Out')
+  describe 'facebook omniauth' do
+    it 'can sign in user with Facebook account' do
+      visit '/'
+      expect(page).to have_content('Sign in with Facebook')
+      mock_auth_facebook
+      click_link 'Sign in with Facebook'
+      expect(page).to have_content('Successfully authenticated from Facebook account.')
+      expect(page).to have_content('Log Out')
+    end
+
+    it 'can handle authentication error' do
+      OmniAuth.config.mock_auth[:facebook] = :invalid_credentials
+      visit '/'
+      expect(page).to have_content('Sign in with Facebook')
+      click_link 'Sign in with Facebook'
+      expect(page).to have_content('Invalid credentials')
+    end
   end
 
-  it 'can handle authentication error' do
-    OmniAuth.config.mock_auth[:twitter] = :invalid_credentials
-    visit '/'
-    expect(page).to have_content('Sign in with Twitter')
-    click_link 'Sign in with Twitter'
-    expect(page).to have_content('Invalid credentials')
+  describe 'instagram omniauth' do
+    it 'can sign in user with Instagram account' do
+      visit '/'
+      expect(page).to have_content('Sign in with Instagram')
+      mock_auth_instagram
+      click_link 'Sign in with Instagram'
+      expect(page).to have_content('Successfully authenticated from Instagram account.')
+      expect(page).to have_content('Log Out')
+    end
+
+    it 'can handle authentication error' do
+      OmniAuth.config.mock_auth[:instagram] = :invalid_credentials
+      visit '/'
+      expect(page).to have_content('Sign in with Instagram')
+      click_link 'Sign in with Instagram'
+      expect(page).to have_content('Invalid credentials')
+    end
   end
 
+  describe 'google omniauth' do
+    it 'can sign in user with Google account' do
+      visit '/'
+      expect(page).to have_content('Sign in with Google Oauth2')
+      mock_auth_google
+      click_link 'Sign in with Google Oauth2'
+      expect(page).to have_content('Successfully authenticated from Google_oauth2 account.')
+      expect(page).to have_content('Log Out')
+    end
+
+    it 'can handle authentication error' do
+      OmniAuth.config.mock_auth[:google_oauth2] = :invalid_credentials
+      visit '/'
+      expect(page).to have_content('Sign in with Google Oauth2')
+      click_link 'Sign in with Google Oauth2'
+      expect(page).to have_content('Invalid credentials')
+    end
+  end
 end
--- spec/support/omniauth_macros.rb
+++ spec/support/omniauth_macros.rb
@@ -1,7 +1,5 @@
 module OmniauthMacros
-  def mock_auth_hash
-    # The mock_auth configuration allows you to set per-provider (or default)
-    # authentication hashes to return during integration testing.
+  def mock_auth_twitter
     OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
       'provider' => 'twitter',
       'uid' => '123545',
@@ -19,4 +17,62 @@ module OmniauthMacros
       },
     })
   end
+
+  def mock_auth_facebook
+    OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
+      'provider' => 'facebook',
+      'uid' => '123545',
+      'info' => {
+        'name' => 'mockuser',
+        'email' => 'mock@example.com',
+        'nickname' => 'mocky',
+        'image' => 'mock_user_thumbnail_url',
+        'phone' => '1234567890',
+      },
+      'credentials' => {
+        'token' => 'mock_token',
+        'refresh_token' => 'mock_refresh',
+        'secret' => 'mock_secret'
+      },
+    })
+  end
+
+  def mock_auth_instagram
+    OmniAuth.config.mock_auth[:instagram] = OmniAuth::AuthHash.new({
+      'provider' => 'instagram',
+      'uid' => '123545',
+      'info' => {
+        'name' => 'mockuser',
+        'email' => 'mock@example.com',
+        'nickname' => 'mocky',
+        'image' => 'mock_user_thumbnail_url',
+        'phone' => '1234567890',
+      },
+      'credentials' => {
+        'token' => 'mock_token',
+        'refresh_token' => 'mock_refresh',
+        'secret' => 'mock_secret'
+      },
+    })
+  end
+
+  def mock_auth_google
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+      'provider' => 'google_oauth2',
+      'uid' => '123545',
+      'info' => {
+        'name' => 'mockuser',
+        'email' => 'mock@example.com',
+        'nickname' => 'mocky',
+        'image' => 'mock_user_thumbnail_url',
+        'phone' => '1234567890',
+      },
+      'credentials' => {
+        'token' => 'mock_token',
+        'refresh_token' => 'mock_refresh',
+        'secret' => 'mock_secret'
+      },
+    })
+  end
+
 end
