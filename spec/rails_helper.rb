c4e75a31939f15b53b2748a6e588408f24097314
Raymond Sapida <ray.sapida@gmail.com>
2015-05-15 09:21:05 -0700

    Add smoke tests for admin resources controllers
    - All tests pass individually and when using `rspec spec` but the
      admin tests fails when using `rake spec`.
    - Rake spec does not load test environment unless you use `RAILS_ENV=test rake
      spec` and that is why it was failing.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,7 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Paperclip::Shoulda::Matchers
-  config.include Devise::TestHelpers, :type => :controller
+  config.include Devise::TestHelpers, type: :controller
   config.include(OmniauthMacros)
 
   config.use_transactional_fixtures = false
