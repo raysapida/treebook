9179d89c2813e38ea76ed5bc7176a3d4cac68947
Raymond Sapida <ray.sapida@gmail.com>
2015-05-15 09:21:05 -0700

    Add smoke tests for admin resources controllers
    - Weird bug: All tests pass individually and when using `rspec spec` but the
      admin tests fails when using `rake spec`.

---
+++ spec/controllers/admin/user_friendships_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Admin::UserFriendshipsController do
+  before do
+    @user = create(:user, email: 'admin@example.com')
+    sign_in :user, @user
+  end
+
+  describe 'GET index' do
+    it 'is successful' do
+      get :index
+      expect(response).to be_success
+    end
+  end
+end
