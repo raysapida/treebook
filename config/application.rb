26f43d146597652d0c6b4d870a01bde0cb08c3f0
Raymond Sapida <ray.sapida@gmail.com>
2026-02-23 17:42:34 -0800

    Update to use 7.0 defaults and delet old initiliazer files

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,11 @@ Bundler.require(*Rails.groups)
 module SocialMedia
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
+    config.load_defaults 7.0
+
+    # Must be set in application.rb, not in new_framework_defaults_7_0.rb.
+    config.active_support.disable_to_s_conversion = true
+    config.active_support.cache_format_version = 7.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
